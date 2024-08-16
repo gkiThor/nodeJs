60f7d17015c5022d6277772bc89f7eb97e46977a
Guillaume Soulichanh <g.soulichanh@gmail.com>
2024-08-16 11:00:10 +0200

    recuperer un parametre passe dans url

--- serveur.js
+++ serveur.js
@@ -4,17 +4,30 @@ fs = require("fs");
 server = http.createServer();
 
 server.on("request",(req,res)=>{
+
     console.log("requete recu");
-    fs.readFile
-    res.writeHead(200,{
-        "content-type":"text/html;charset=utf-8"
-    });
-    res.write("<b>Bonjour</b> de la part de nodejs");
-    // res.writeHead(210,{
-    //     "content-type":"text/json"
-    // });
-    // res.write('"nom":"kithor","prenom":"souli"');
-    res.end();
+    console.log(req.url);//recuperer les parametres passe dans url
+
+    tab=req.url.split("=");
+    console.log(tab[1]);
+
+    fs.readFile("index.html","utf-8",(err,data)=>{
+        if(err){
+            res.writeHead(404);
+            res.write("Document introuvable");
+        }
+        else{
+            res.writeHead(200,{
+                "content-type":"text/html;charset=utf-8"
+            });
+            res.write(data);
+            // res.writeHead(210,{
+            //     "content-type":"text/json"
+            // });
+            // res.write('"nom":"kithor","prenom":"souli"');
+        }
+        res.end();
+    }) 
 })
 
 server.listen(8080,()=>{
